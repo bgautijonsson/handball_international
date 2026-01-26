3f25bf69c456afb43564b7c51c3eccf0b0615f83
bgautijonsson <bgautijonsson@gmail.com>
2026-01-26 20:26:48 +0000

    First commit of full repo except for model rds files

---
+++ R/update_model.R
@@ -0,0 +1,18 @@
+box::use(
+  R / common / model_fitting[fit_handball_model],
+  R / common / get_model_results[generate_model_results]
+)
+
+end_date <- Sys.Date()
+sex <- "male"
+
+fit_handball_model(
+  sex = sex,
+  model_name = "2d_student_t_matchup_effects.stan",
+  refresh = 100,
+  iter_warmup = 200,
+  iter_sampling = 200,
+  end_date = end_date
+)
+
+generate_model_results("male", end_date = end_date)
